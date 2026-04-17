32de27408fbcf676f18640bbcbdd1d4ed18900c1
Glyn Gray <glyn@stirstudiosdesign.com>
2026-04-17 10:27:14 -0700

    Fix extraneous attrs warning on BlockContentParagraph

--- app/components/global/BlockContentParagraph.vue
+++ app/components/global/BlockContentParagraph.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+
 defineProps<{
+  id?: number | string
+  uuid?: string
+  parentUuid?: string
+  editLink?: string
   title?: string
   type?: string
   created?: number | string
