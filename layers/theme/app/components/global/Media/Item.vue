ef4bea434b7bd3471ba93d0eaa8078c1838d427d
Glyn Gray <glyn@stirstudiosdesign.com>
2026-06-26 16:47:12 -0700

    fix(media): animate media nodes without video special-case

--- layers/theme/app/components/global/Media/Item.vue
+++ layers/theme/app/components/global/Media/Item.vue
@@ -94,7 +94,6 @@ const revealMotionProps = computed(() =>
 )
 
 const animatedMediaMotionProps = computed<Record<string, unknown>>(() => ({
-  as: mediaComponent.value,
   ...mediaProps.value,
   ...revealMotionProps.value,
   editActions: props.editActions,
@@ -102,7 +101,6 @@ const animatedMediaMotionProps = computed<Record<string, unknown>>(() => ({
 }))
 
 const shouldAnimate = computed(() =>
-  !isVideo.value &&
   Boolean((revealMotionProps.value as Record<string, unknown>)?.whileInView),
 )
 </script>
@@ -119,8 +117,16 @@ const shouldAnimate = computed(() =>
   <Motion
     v-else-if="!overlay || isDocument || isAudio"
     :key="`media-${props.index}-${revealMotionKey}`"
+    as-child
     v-bind="animatedMediaMotionProps"
-  />
+  >
+    <component
+      :is="mediaComponent"
+      v-bind="mediaProps"
+      :edit-actions="editActions"
+      @edit-action-select="handleEditActionSelect"
+    />
+  </Motion>
 
   <Motion
     v-else
