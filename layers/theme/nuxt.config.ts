251e6ab18cf2bfdf2cac6c732755094703a8dbcc
Glyn Gray <glyn@stirstudiosdesign.com>
2026-06-30 19:46:17 -0700

    Resolve layered theme CSS entry

--- layers/theme/nuxt.config.ts
+++ layers/theme/nuxt.config.ts
@@ -1,8 +1,3 @@
-import { dirname, resolve as resolvePath } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const themeLayerDir = dirname(fileURLToPath(import.meta.url))
-
 export default defineNuxtConfig({
-  css: [resolvePath(themeLayerDir, 'app/assets/css/main.css')],
+  css: ['~/assets/css/main.css'],
 })
